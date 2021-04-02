95d1723e271d1ae22654c4d4d253d080a4f3ca83
smalaca <ab13.krakow@gmail.com>
2021-04-02 15:49:45 +0200

    Spring - Kafka - Communication - read header and send own headers

--- spring-kafka-communication/src/main/java/com/smalaca/spring/kafka/consumer/MessageConsumer.java
+++ spring-kafka-communication/src/main/java/com/smalaca/spring/kafka/consumer/MessageConsumer.java
@@ -1,12 +1,17 @@
 package com.smalaca.spring.kafka.consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 class MessageConsumer {
     @KafkaListener(
-            topics = {"${topics.topic-one}", "${topics.topic-two}", "${topics.topic-three}", "${topics.topic-four}", "${topics.topic-five}"})
+            topics = {"${topics.topic-one}", "${topics.topic-two}", "${topics.topic-three}", "${topics.topic-four}"})
     public void consumeOne(String message) {
         System.out.println("DEFAULT; " + message);
     }
@@ -19,5 +24,16 @@ class MessageConsumer {
     @KafkaListener(topics = {"${topics.topic-one}"}, groupId = "group-two")
     public void consumerThree(String message) {
         System.out.println("group-two; " + message);
+    }
+
+    @KafkaListener(topics = {"${topics.topic-five}"}, groupId = "group-three")
+    public void consumerFour(
+            @Payload String message,
+            @Header("my-header") String myHeader,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partitionId,
+            @Headers MessageHeaders messageHeaders) {
+        System.out.println("group-three; " + message + "; " + partitionId);
+        System.out.println("HEADERS with my header: " + myHeader);
+        messageHeaders.forEach((s, s2) -> System.out.println(s + ": " + s2));
     }
 }
--- spring-kafka-communication/src/main/java/com/smalaca/spring/kafka/producer/OnStartupProducer.java
+++ spring-kafka-communication/src/main/java/com/smalaca/spring/kafka/producer/OnStartupProducer.java
@@ -1,8 +1,12 @@
 package com.smalaca.spring.kafka.producer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -12,17 +16,36 @@ import java.util.stream.IntStream;
 class OnStartupProducer {
     private final KafkaTemplate<String, String> template;
     private final KafkaTopics kafkaTopics;
+    private final String topicName;
 
-    OnStartupProducer(KafkaTemplate<String, String> template, KafkaTopics kafkaTopics) {
+    OnStartupProducer(
+            KafkaTemplate<String, String> template, KafkaTopics kafkaTopics, @Value("${topics.topic-five}") String topicName) {
         this.template = template;
         this.kafkaTopics = kafkaTopics;
+        this.topicName = topicName;
     }
 
     @Async
     @EventListener
-    public void listen(ContextRefreshedEvent event) {
+    public void listenerOne(ContextRefreshedEvent event) {
         IntStream.range(0, 10).forEach(index -> {
-            kafkaTopics.forEach(topicName -> template.send(topicName, message(index, topicName)));
+            kafkaTopics.forEach(topicName -> {
+                if (!this.topicName.equals(topicName)) {
+                    template.send(topicName, message(index, topicName));
+                }
+            });
+        });
+    }
+
+    @Async
+    @EventListener
+    public void listenerTwo(ContextRefreshedEvent event) {
+        IntStream.range(0, 10).forEach(index -> {
+            Message<String> message = MessageBuilder.withPayload("With HEADER" + message(index, topicName))
+                    .setHeader(KafkaHeaders.TOPIC, topicName)
+                    .setHeader("my-header", "header value " + index)
+                    .build();
+            template.send(message);
         });
     }
 
