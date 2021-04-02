3177b139bab75827458ef200be5be5367df7cfd6
smalaca <ab13.krakow@gmail.com>
2021-04-02 15:21:35 +0200

    Spring - Kafka - Communication - add consumers from different group

--- spring-kafka-communication/src/main/java/com/smalaca/spring/kafka/consumer/MessageConsumer.java
+++ spring-kafka-communication/src/main/java/com/smalaca/spring/kafka/consumer/MessageConsumer.java
@@ -7,7 +7,17 @@ import org.springframework.stereotype.Component;
 class MessageConsumer {
     @KafkaListener(
             topics = {"${topics.topic-one}", "${topics.topic-two}", "${topics.topic-three}", "${topics.topic-four}", "${topics.topic-five}"})
-    public void consume(String message) {
-        System.out.println(message);
+    public void consumeOne(String message) {
+        System.out.println("DEFAULT; " + message);
+    }
+
+    @KafkaListener(topics = {"${topics.topic-one}"}, groupId = "group-two")
+    public void consumerTwo(String message) {
+        System.out.println("group-one; " + message);
+    }
+
+    @KafkaListener(topics = {"${topics.topic-one}"}, groupId = "group-two")
+    public void consumerThree(String message) {
+        System.out.println("group-two; " + message);
     }
 }
